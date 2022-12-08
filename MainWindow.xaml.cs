1e2b3d644cdc15d0aa76d5d9a7f9eed097db7ea6
исп-31 <-31@LAB17-07>
2022-12-08 13:42:16 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using Pr9v7;
+
+
+namespace Pr9v7
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public PersonData Person { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void mItAboutProgram_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Вариант 7. " +
+                "\r\nЗаполнить таблицу со списком телефонных абонентов " +
+                "\r\nна 7 человек с полями: ФИО, номер телефона, адрес. " +
+                "\r\nВывести результат на экран. " +
+                "\r\nВывести список абонентов живущих на заданной улице",
+                "Разработчик: Десятов",
+                MessageBoxButton.OK,
+                MessageBoxImage.Asterisk);
+        }
+
+        private void mItExit_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        int count = 0;
+
+        /// <summary>
+        /// Заполнение таблицы данными 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnFill_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string firstName = txtBoxFirstName.Text;
+                string patronomic = txtBoxPatronymic.Text;
+                string lastName = txtBoxLastName.Text;
+                int phoneNumber = Convert.ToInt32(txtBoxPhoneNumber.Text);
+                string address = txtBoxAddress.Text;
+                Person = new PersonData(firstName, patronomic, lastName, phoneNumber, address);
+                dGFillData.Items.Add(Person);
+                count++;
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("Неверные данные",
+                    "Ошибка",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Error);
+            }
+        }
+
+
+        /// <summary>
+        /// Вывод списка абонентов по указанному адресу
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnResult_Click(object sender, RoutedEventArgs e)
+        {
+
+
+            if (dGFillData.Items.Count == 0)
+            {
+                MessageBox.Show("Таблица пустая, заполните значениями",
+                    "Ошибка",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Error);
+                return;
+            }
+
+            string result = " ";
+
+            PersonData[] personData = new PersonData[count];
+
+            for (int i = 0; i < dGFillData.Items.Count; i++)
+            {
+                personData[i] = (PersonData)dGFillData.Items[i];
+            }
+
+
+            for (int i = 0; i < personData.Length; i++) 
+            {
+                if (personData[i]._address == txtBoxSelectedAddress.Text)
+                {
+                    result += $"{personData[i]._firstName}\r\n";
+                }
+            }
+
+            txtBoxResult.Text = result;
+        }
+    }
+}
---
+++ PersonData.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pr9v7
+{
+    /// <summary>
+    /// Структура для работы с данными пользователя
+    /// </summary>
+    public struct PersonData
+    {
+        /// <summary>
+        /// Поля структуры PersonData
+        /// </summary>
+        public string _firstName { get; set; }
+        public string _patronomic { get; set; }
+        public string _lastName { get; set; }
+        public int _phoneNumber { get; set; }
+        public string _address { get; set; }
+
+        /// <summary>
+        /// Конструктор структуры PersonData
+        /// </summary>
+        /// <param name="firsName"></param>
+        /// <param name="patronomic"></param>
+        /// <param name="lastName"></param>
+        /// <param name="phoneNumber"></param>
+        /// <param name="address"></param>
+        public PersonData (string firsName, string patronomic, string lastName, int phoneNumber, string address)
+        {
+            _firstName = firsName;
+            _patronomic = patronomic;
+            _lastName = lastName;
+            _phoneNumber = phoneNumber;
+            _address = address;
+        }        
+    }
+}
